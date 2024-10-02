1bf341acb24f3f6624fc7a35811cc38f9a21c576
Mike Goodstein <75456602+apathetichell@users.noreply.github.com>
2024-10-01 22:35:11 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,21 +7,7 @@ terraform {
       docker = {
       source  = "kreuzwerker/docker"
       version = "3.0.2"
-    }
-
-
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.87"
-    }    
+    }   
   }
   required_version = "~> 1.7"
-
-  backend "s3" {
-    bucket         = "mg-tf-state-backend"
-    key            = "state/terraform.bedrock-tf-agent"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "my-terraform-lockdb"
   }
-}
